6ff216dee7fe52fe51f77363b23c9a9c3f224564
NidhiSBC <nidhi@sbcel.com>
2026-05-24 00:50:42 +0530

    fix: invalid GUID format in Interest seed data

--- Mingley.API/Program.cs
+++ Mingley.API/Program.cs
@@ -104,7 +104,9 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<MingleyDbContext>();
     try
     {
-        db.Database.EnsureCreated();
+        //db.Database.EnsureCreated();
+        await db.Database.MigrateAsync();
+
         Log.Information("✅ Database ready");
     }
     catch (Exception ex)
